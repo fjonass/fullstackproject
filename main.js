c2a3629f43721d63794f4b507e6112ca877b6e5b
fjonass <jsabini@pdx.edu>
2021-03-05 21:47:22 -0800

    search is done
    search function is done

--- main.js
+++ main.js
@@ -3,7 +3,10 @@ const app = express();
 const session = require('express-session'); // npm install express-session
 const path = require('path'); // npm install path
 const fs = require('fs'); // npm install fs
+const parser = require('body-parser');
 const port = process.env.PORT || 5001;
+var sea = '';
+var sea2 = '';
 
 // Template ejs
 app.set('views', __dirname + '');
@@ -46,6 +49,63 @@ app.get('/cartPage', (req, res) => {
         
     });
 });
+
+app.get('/search', (req, res) => {
+
+    var items = data;
+    
+    // change data in itemListPage.ejs file and display it
+    res.render('itemListPageSearch', {
+        items: items,
+        name: sea2
+    });
+});
+
+
+app.use(express.urlencoded({
+    extended: true
+  }))
+
+app.post('/search', (req, res) => {
+    //res.writeHead(200, { 'Content-Type': 'text/html' });
+    console.log(`search: ${req.body.cari}`);
+    var seaArr = req.body.cari.toLowerCase();
+    sea = '/itemListPage' + seaArr;
+
+    if(
+    seaArr == 'electronic'|| 
+    seaArr == 'toys'||
+    seaArr == 'outdoor'||
+    seaArr == 'fashion'||
+    seaArr == 'office'||
+    seaArr == 'personal_care'||
+    seaArr == 'pets'||
+    seaArr == 'books'||
+    seaArr == 'grocery'
+    )
+    res.redirect(sea);
+    
+    else
+    {
+        sea2 = req.body.cari;
+        res.redirect('/search');
+    }
+
+   /* res.write(`<br>`);
+    res.write(`Email: ${req.body.Emaill}`);
+    res.write(`<br>`);
+    if(req.body.subject)
+        res.write(`Comment: ${req.body.subject}`);
+    else
+        res.write(`Comment: n/a`);
+    res.write(`<br>`);
+    if(req.body.check)
+        res.write(`Newsletter: Yes, I would like to sign up for the newsletter.`);
+    else
+        res.write(`No, thank you.`);*/
+    res.end();
+});
+
 
 app.listen(port, () => {
     console.log('Server is running at http://localhost:5001...');
