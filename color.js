cfb8213cc3c39452e6052e04733b06120b723810
fjonass <jsabini@pdx.edu>
2021-03-13 00:18:18 -0800

    darklight button align

--- color.js
+++ color.js
@@ -1,9 +1,18 @@
-//const colorScheme = localStorage.getItem("color-scheme") || colorScheme;
 
-//document.getElementById("light-mode").checked = colorScheme === "light-mode";
-//document.getElementById("dark-mode").checked = colorScheme === "dark-mode";
 
-Array.from(document.querySelectorAll("input[name='color-scheme']"))
-	.forEach(el => el.addEventListener("click", event => {
-		localStorage.setItem("color-scheme", event.target.id);
-	}));
+
+//var colorScheme = localStorage.setItem("color-scheme","dark-mode");
+
+//Array.from(document.querySelectorAll("input[name='color-scheme']"))
+//	.forEach(el => el.addEventListener("click", event => {
+//		localStorage.setItem("color-scheme", "light-mode");
+		//console.log(event.target.id);
+//	}));
+
+/*
+<script> 
+    document.getElementById("light-mode").checked = colorScheme === "light-mode";
+    document.getElementById("dark-mode").checked = colorScheme === "dark-mode";
+    const colorScheme = localStorage.getItem("color-scheme") || colorScheme
+</script>
+*/
